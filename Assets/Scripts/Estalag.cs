59fb4bdacbb344b9b0bdc8506d997538b1b96c63
xRaTiOnX <jordanhgoncalves@gmail.com>
2018-06-30 02:06:24 -0300

    UC7A2

---
+++ Assets/Scripts/Estalag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Estalag : MonoBehaviour
+{
+    public Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if(Trig1.range == true)
+        {
+            Trap1.lives -= 1;
+            Destroy(col.gameObject);
+            SceneManager.LoadScene("Jogo");
+        }
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public static int scorevalue = 0;
+    public Text scoring;
+
+
+	void Start () {
+        scoring = GetComponent<Text>();
+	}
+	
+	void Update () {
+        scoring.text = "Score: " + scorevalue;
+        if (RotacionaPedra.scorepedra == 1)
+        {
+            scorevalue += 100;
+            RotacionaPedra.scorepedra = 0;
+        }
+        if (Trap1.lives == 1 ||Trap1.lives == 0)
+        {
+            scorevalue = 0;
+        }
+    }
+}
